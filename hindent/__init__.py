44d864885fb72c2c0da859521991c357a7a36d9f
GSmithApps <14.gsmith.14@gmail.com>
2023-12-12 21:37:01 -0600

    moved execution code

--- hindent/__init__.py
+++ hindent/__init__.py
@@ -2,7 +2,10 @@
 Provides functions to translate and/or execute Hindent code.
 
 >>> import hindent as h
->>> h.run_file('./your-file.hin')
+>>> h.translate_file('./your-file.hin')
+(
+println (
+  + 2 2 ) )
 
 or
 
@@ -11,16 +14,13 @@ or
 ... println
 ...   + 2 2
 ... '''
->>> h.run_string(hindent_code)
+>>> h.translate_string(hindent_code)
+(
+println (
+  + 2 2 ) )
 
 Provided functions
 ------------------
-
-run_file(file_path: Path)
-    Executes and retrieves the output of Lisp code from a specified file.
-
-run_string(hindent_code: str)
-    Executes and retrieves the output of Lisp code from a given string.
 
 translate_file(file_path: Path)
     Translates Hindent code from a specified file.
@@ -28,186 +28,20 @@ translate_file(file_path: Path)
 translate_string(hindent_code: str)
     Translates Hindent code by adding appropriate parentheses based on indentation.
 
-execute_lisp(lisp_code: str)
-    Executes the given lisp code.
+get_text_from_file_path(file_path: Path)
+    Reads text from a file specified by the given file path.
 
-Using a custom Lisp executor
-----------------------------
+Additional Modules
+------------------
 
-The default Lisp executor uses clojure. However, you can
-use a custom Lisp executor by setting the `lisp_executor` variable to a function
-that takes a filename as input, executes the Lisp code in the file, and returns the
-output and error of the execution as a tuple.
+lisp_execution
+    Provides functions to execute Lisp code.
+
 """
 
 __version__ = "4.0.0"
 
 from pathlib import Path
-import os
-import subprocess
-
-
-_SAVED_FILE_TXT = "SAVED_FILE.txt"
-
-
-# This class is just for type hinting for a function/callback
-class LispExecutor:
-    """
-    A callback function type that executes Lisp code from a given filename.
-
-    This is intended to be used as a function type for custom executors.
-
-    The function should take a filename as input, execute the Lisp code in the file, and
-    return the output and error of the execution as a tuple.
-
-    Parameters
-    ----------
-    filename : str
-        The name of the file to execute.
-
-    Returns
-    -------
-
-    tuple
-        The output and error of the execution.
-
-    Examples
-    --------
-
-    >>> def _clojure_executor(filename):
-    ...     process = subprocess.run(
-    ...         ["clojure", filename], capture_output=True, text=True
-    ...     )
-    ...     return process.stdout, process.stderr
-    """
-
-
-def _scheme_executor(filename):
-    """
-    Default executor function for executing Scheme code using Chez Scheme.
-
-    This function executes Scheme code from a specified file using Chez Scheme, capturing
-    and returning the standard output and standard error.
-
-    Parameters
-    ----------
-    filename : str
-        The path to the Scheme file to be executed.
-
-    Returns
-    -------
-    tuple
-        A tuple containing two elements: the standard output and standard error from the executed Scheme code.
-
-    Examples
-    --------
-    >>> stdout, stderr = _default_lisp_executor("example.scm")
-    >>> print(stdout)
-    """
-
-    # For Chez Scheme, the command is typically 'scheme --script'
-    process = subprocess.run(
-        ["chez", "--script", filename], capture_output=True, text=True
-    )
-    return process.stdout, process.stderr
-
-
-def _clojure_executor(filename):
-    """
-    Executor function for executing Clojure code using Clojure.
-
-    This function executes Clojure code from a specified file using Clojure, capturing
-    and returning the standard output and standard error.
-
-    Parameters
-    ----------
-    filename : str
-        The path to the Clojure file to be executed.
-
-    Returns
-    -------
-    tuple
-        A tuple containing two elements: the standard output and standard error from the executed Clojure code.
-
-    Examples
-    --------
-    >>> stdout, stderr = _clojure_executor("example.clj")
-    >>> print(stdout)
-    """
-
-    # For Clojure, the command is typically 'clojure'
-    process = subprocess.run(
-        ["clojure", "-M", filename], capture_output=True, text=True
-    )
-    return process.stdout, process.stderr
-
-
-lisp_executor: LispExecutor = _clojure_executor
-
-
-def run_file(file_path: Path) -> tuple:
-    """
-    Executes and retrieves the output of Lisp code from a specified file.
-
-    This method allows the user to run Lisp code from a file of their choice,
-    rather than the default file set during initialization.
-
-    Parameters
-    ----------
-    file_path : Path
-        The path to the file containing the Hindent code to be formatted and executed.
-
-    Returns
-    -------
-    tuple
-        A tuple containing the standard output and standard error from the executed Lisp code.
-
-    >>> import hindent as h
-    >>> h.run_file('./example_hindent_code.hin')
-    some output
-    """
-    return run_string(
-        _get_text_from_file_path(file_path),
-    )
-
-
-def run_string(hindent_code: str) -> tuple:
-    """
-    Executes and retrieves the output of Lisp code from a given string.
-
-    This method allows the user to run Lisp code directly from a string,
-    enabling on-the-fly execution without needing to write to a file.
-
-    Parameters
-    ----------
-    hindent_code : str
-        The string containing the Hindent code to be formatted and executed.
-
-    Returns
-    -------
-    tuple
-        A tuple containing the standard output and standard error from the executed Lisp code.
-
-    Examples
-    --------
-
-    >>> import hindent as h
-    >>> hindent_code = '''
-    ... println
-    ...   + 2 2
-    ... '''
-    >>> h.run_string(hindent_code)
-    some output
-    """
-
-    # Add parentheses with the fixed function and return the result
-    parenthesized_code_fixed = translate_string(hindent_code)
-
-    # Example usage
-    output, error = execute_lisp(parenthesized_code_fixed)
-
-    return output, error
-
 
 
 def translate_file(file_path: Path) -> str:
@@ -234,7 +68,7 @@ def translate_file(file_path: Path) -> str:
     >>> print(lisp_code)
     """
     return translate_string(
-        _get_text_from_file_path(file_path),
+        get_text_from_file_path(file_path),
     )
 
 
@@ -276,7 +110,6 @@ def translate_string(hindent_code: str) -> str:
 
     # remove comment blocks and whole line comments.
     for i, line in enumerate(lines):
-
         # Handle code/comment blocks
         if line.rstrip() == ",":
             in_code_block = not in_code_block
@@ -292,7 +125,9 @@ def translate_string(hindent_code: str) -> str:
 
         validlines.append(line)
 
-    validlines.append("\n")  # Add a newline to the end to ensure the final outdent is correct
+    validlines.append(
+        "\n"
+    )  # Add a newline to the end to ensure the final outdent is correct
 
     # Function to calculate the indentation level of a line
     def indentation_level(line):
@@ -302,7 +137,6 @@ def translate_string(hindent_code: str) -> str:
     processed_lines = []
 
     for i, line in enumerate(validlines[:-1]):
-
         next_line = validlines[i + 1]
 
         # Remove any end-of-line comments
@@ -325,7 +159,8 @@ def translate_string(hindent_code: str) -> str:
         # Determine the required parentheses
         if (
             # current line is totally blank with no indentation or period
-            (current_indent == 0 and line.strip() == "") and
+            (current_indent == 0 and line.strip() == "")
+            and
             # the next line is anything but blank
             not (next_indent == 0 and next_line.strip() == "")
         ):
@@ -333,14 +168,15 @@ def translate_string(hindent_code: str) -> str:
 
         if (
             # current line is anything but blank
-            not (current_indent == 0 and line.strip() == "") and
+            not (current_indent == 0 and line.strip() == "")
+            and
             # next line is blank
             (next_indent == 0 and next_line.strip() == "")
         ):
             line = line.rstrip() + " )"
 
         if next_indent > current_indent:
-            line = line.rstrip() + " " + ("("  * (next_indent - current_indent)) 
+            line = line.rstrip() + " " + ("(" * (next_indent - current_indent))
         elif next_indent < current_indent:
             line = line.rstrip() + " " + (")" * (current_indent - next_indent))
 
@@ -350,38 +186,7 @@ def translate_string(hindent_code: str) -> str:
     return "\n".join(processed_lines)
 
 
-def _save_code_to_file(code, filename):
-    with open(filename, "w") as file:
-        file.write(code)
-
-
-def execute_lisp(lisp_code: str) -> tuple:
-    """
-    Executes the given lisp code.
-
-    Parameters
-    ----------
-    parenthesized_code_fixed : str
-        The lisp code to be executed.
-
-    Returns
-    -------
-    tuple
-        A tuple containing the standard output and standard error from the executed lisp code.
-    """
-
-    _save_code_to_file(lisp_code, _SAVED_FILE_TXT)
-    output, error = lisp_executor(_SAVED_FILE_TXT)
-
-    print(output)
-    print(error)
-
-    # delete file
-    os.remove(_SAVED_FILE_TXT)
-    return output, error
-
-
-def _get_text_from_file_path(file_path: Path) -> str:
+def get_text_from_file_path(file_path: Path) -> str:
     """
     Reads text from a file specified by the given file path.
 
@@ -443,7 +248,5 @@ def _remove_potential_end_of_line_comment(line: str) -> str:
 
         if char == ";" and not in_string:
             return line[:i]
-    
-    return line
 
-            
+    return line
---
+++ hindent/execution.py
@@ -0,0 +1,223 @@
+"""
+Provides functions for executing Hindent (or other) code.
+
+Usually used to 
+
+Provided functions
+------------------
+
+run_file(file_path: Path)
+    Executes and retrieves the output of Lisp code from a specified file.
+
+run_string(hindent_code: str)
+    Executes and retrieves the output of Lisp code from a given string.
+
+execute_lisp(lisp_code: str)
+    Executes the given lisp code.
+
+Using a custom Lisp executor
+----------------------------
+
+The default Lisp executor uses clojure. However, you can
+use a custom Lisp executor by setting the `lisp_executor` variable to a function
+that takes a filename as input, executes the Lisp code in the file, and returns the
+output and error of the execution as a tuple.
+"""
+
+import subprocess
+import os
+from pathlib import Path
+from hindent import translate_string, get_text_from_file_path
+
+
+_SAVED_FILE_TXT = "SAVED_FILE.txt"
+
+
+# This class is just for type hinting for a function/callback
+class LispExecutor:
+    """
+    A callback function type that executes Lisp code from a given filename.
+
+    This is intended to be used as a function type for custom executors.
+
+    The function should take a filename as input, execute the Lisp code in the file, and
+    return the output and error of the execution as a tuple.
+
+    Parameters
+    ----------
+    filename : str
+        The name of the file to execute.
+
+    Returns
+    -------
+
+    tuple
+        The output and error of the execution.
+
+    Examples
+    --------
+
+    >>> def _clojure_executor(filename):
+    ...     process = subprocess.run(
+    ...         ["clojure", filename], capture_output=True, text=True
+    ...     )
+    ...     return process.stdout, process.stderr
+    """
+
+
+def _scheme_executor(filename):
+    """
+    Default executor function for executing Scheme code using Chez Scheme.
+
+    This function executes Scheme code from a specified file using Chez Scheme, capturing
+    and returning the standard output and standard error.
+
+    Parameters
+    ----------
+    filename : str
+        The path to the Scheme file to be executed.
+
+    Returns
+    -------
+    tuple
+        A tuple containing two elements: the standard output and standard error from the executed Scheme code.
+
+    Examples
+    --------
+    >>> stdout, stderr = _default_lisp_executor("example.scm")
+    >>> print(stdout)
+    """
+
+    # For Chez Scheme, the command is typically 'scheme --script'
+    process = subprocess.run(
+        ["chez", "--script", filename], capture_output=True, text=True
+    )
+    return process.stdout, process.stderr
+
+
+def _clojure_executor(filename):
+    """
+    Executor function for executing Clojure code using Clojure.
+
+    This function executes Clojure code from a specified file using Clojure, capturing
+    and returning the standard output and standard error.
+
+    Parameters
+    ----------
+    filename : str
+        The path to the Clojure file to be executed.
+
+    Returns
+    -------
+    tuple
+        A tuple containing two elements: the standard output and standard error from the executed Clojure code.
+
+    Examples
+    --------
+    >>> stdout, stderr = _clojure_executor("example.clj")
+    >>> print(stdout)
+    """
+
+    # For Clojure, the command is typically 'clojure'
+    process = subprocess.run(
+        ["clojure", "-M", filename], capture_output=True, text=True
+    )
+    return process.stdout, process.stderr
+
+
+lisp_executor: LispExecutor = _clojure_executor
+
+
+def run_file(file_path: Path) -> tuple:
+    """
+    Executes and retrieves the output of Lisp code from a specified file.
+
+    This method allows the user to run Lisp code from a file of their choice,
+    rather than the default file set during initialization.
+
+    Parameters
+    ----------
+    file_path : Path
+        The path to the file containing the Hindent code to be formatted and executed.
+
+    Returns
+    -------
+    tuple
+        A tuple containing the standard output and standard error from the executed Lisp code.
+
+    >>> import hindent as h
+    >>> h.run_file('./example_hindent_code.hin')
+    some output
+    """
+    return run_string(
+        get_text_from_file_path(file_path),
+    )
+
+
+def run_string(hindent_code: str) -> tuple:
+    """
+    Executes and retrieves the output of Lisp code from a given string.
+
+    This method allows the user to run Lisp code directly from a string,
+    enabling on-the-fly execution without needing to write to a file.
+
+    Parameters
+    ----------
+    hindent_code : str
+        The string containing the Hindent code to be formatted and executed.
+
+    Returns
+    -------
+    tuple
+        A tuple containing the standard output and standard error from the executed Lisp code.
+
+    Examples
+    --------
+
+    >>> import hindent as h
+    >>> hindent_code = '''
+    ... println
+    ...   + 2 2
+    ... '''
+    >>> h.run_string(hindent_code)
+    some output
+    """
+
+    # Add parentheses with the fixed function and return the result
+    parenthesized_code_fixed = translate_string(hindent_code)
+
+    # Example usage
+    output, error = execute_lisp(parenthesized_code_fixed)
+
+    return output, error
+
+
+def _save_code_to_file(code, filename):
+    with open(filename, "w") as file:
+        file.write(code)
+
+
+def execute_lisp(lisp_code: str) -> tuple:
+    """
+    Executes the given lisp code.
+
+    Parameters
+    ----------
+    parenthesized_code_fixed : str
+        The lisp code to be executed.
+
+    Returns
+    -------
+    tuple
+        A tuple containing the standard output and standard error from the executed lisp code.
+    """
+
+    _save_code_to_file(lisp_code, _SAVED_FILE_TXT)
+    output, error = lisp_executor(_SAVED_FILE_TXT)
+
+    print(output)
+    print(error)
+
+    # delete file
+    os.remove(_SAVED_FILE_TXT)
+    return output, error
