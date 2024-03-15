8ae1300f77218ca4d3f8266ca364149f02ad74ac
GSmithApps <14.gsmith.14@gmail.com>
2024-03-14 23:07:42 -0500

    good

--- examples.py
+++ examples.py
@@ -1,37 +1,5 @@
 """
-This is a toy language interpreter/transpiler. It translates
-the toy code into
-a snippet of specialized python code. The basic idea is to
-use indentation and curly braces `}` to denote nested parenthesis
-and a call to a `compose` function. For example, I want
 
-```newlang
-} my_func
-  2
-  2
-```
-
-to be converted to
-
-```python
-compose(myfunc, 2, 2,)
-```
-
-and with nesting, we want
-
-```newlang
-} newfunc
-  2
-  } otherfunc
-    3
-    4
-```
-
-to be
-
-```python
-compose(newfunc, 2, compose(otherfunc, 3, 4,),)
-```
 """
 
 from hindent import co, translate_hindent, wrap_in_unit_function, pri, p, our_id, concat, t, run_hindent
--- hindent/__init__.py
+++ hindent/__init__.py
@@ -127,7 +127,41 @@ def co(*fs):
 
 
 def translate_hindent(input_code: str) -> str:
+    """
+    This is a toy language interpreter/transpiler. It translates
+    the toy code into
+    a snippet of specialized python code. The basic idea is to
+    use indentation and curly braces `}` to denote nested parenthesis
+    and a call to a `compose` function. For example, I want
 
+    ```newlang
+    } my_func
+      2
+      2
+    ```
+
+    to be converted to
+
+    ```python
+    compose(myfunc, 2, 2,)
+    ```
+
+    and with nesting, we want
+
+    ```newlang
+    } newfunc
+      2
+      } otherfunc
+        3
+        4
+    ```
+
+    to be
+
+    ```python
+    compose(newfunc, 2, compose(otherfunc, 3, 4,),)
+    ```
+    """
     input_code = replace_whole_word(input_code, "print", "pri")
     input_code = input_code.replace('+', 'p')
     input_code = input_code.replace("*", "t")
